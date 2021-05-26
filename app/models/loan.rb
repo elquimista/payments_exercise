ed1f90b5a639394dffdc66a5ce8cfcfa7e6be331
Vin Kim <vinkim002@gmail.com>
2021-05-26 10:49:06 -0400

    Add attribute

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,9 @@
 class Loan < ActiveRecord::Base
+  has_many :payments
+
+  attribute :outstanding_balance, :float
+
+  def get_outstanding_balance
+    self["outstanding_balance"] = funded_amount - payments.sum(:amount)
+  end
 end
